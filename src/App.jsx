9ca62b90509890122886af617aa1fccc52f2fdbe
graytrann <trankhaiphong987@gmail.com>
2023-08-09 10:40:31 +0700

    first push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p>Hello Everyone, these are my exercise of React</p>
+    </div>
+  );
+}
+
+export default App;
